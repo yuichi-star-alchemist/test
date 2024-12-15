e8326ae92bf3f67300c3a27b5fd815116f43017b
ゆういち <yuichi-star-alchemist@users.noreply.github.com>
2024-12-15 14:08:00 +0900

    f

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,13 +1,13 @@
 import { cookies } from "next/headers"
 import type { NextRequest } from "next/server"
-import { NextResponse } from "next/server"
 
 export const middleware = async (
   request: NextRequest,
 ) => {
   const cookieStore = await cookies()
   const cookie = cookieStore.get("fantre")
-  if (!cookie) return NextResponse.redirect(new URL("login", request.url))
+  console.log(cookie, request)
+  // if (!cookie) return NextResponse.redirect(new URL("login", request.url))
 }
 
 export const config = {
