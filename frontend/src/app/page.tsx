6b43f8547c4d419a8e1697921ea8181780c1e7b0
ゆういち <yuichi-star-alchemist@users.noreply.github.com>
2024-12-15 15:06:28 +0900

    f

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
+import { LOADING_IMAGE_URL as loading } from "@/constants"
 import TopPage from "@/features/routes/top/components/TopPage"
 import { getImageUrl } from "@/utils/getRequestFromServer"
 
 const initialTopPage = async () => {
   const endpoint = "user/bg-images"
-  const imageUrl = await getImageUrl(endpoint) || ""
-  const imageSrc = imageUrl.split("localhost").join("57.180.44.232") || ""
+  const imageUrl = await getImageUrl(endpoint) || loading
+  const imageSrc = imageUrl.split("localhost").join("57.180.44.232")
   return (
     <TopPage
       imageSrc={ imageSrc }
